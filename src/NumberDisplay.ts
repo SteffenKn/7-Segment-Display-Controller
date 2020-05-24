96ff92df8a5f8034535c1e50cff8e61fc8a7da7d
Steffen <Steffenknaup@hotmail.de>
2020-05-24 23:01:38 +0200

    :bug: Fix displaying 1

--- src/NumberDisplay.ts
+++ src/NumberDisplay.ts
@@ -18,7 +18,7 @@ export class NumberDisplay {
         this.sevenSegmentDisplay.showSegments([0, 1, 2, 4, 5, 6], color);
         break;
       case 1:
-        this.sevenSegmentDisplay.showSegments([1, 2, 6], color);
+        this.sevenSegmentDisplay.showSegments([2, 6], color);
         break;
       case 2:
         this.sevenSegmentDisplay.showSegments([1, 2, 3, 4, 5], color);
